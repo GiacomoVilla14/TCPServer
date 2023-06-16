1b82db8112f75d3ddcdbf50276991bb512ddc2fc
GiacomoVilla14 <g.villa@itsrizzoli.it>
2023-06-16 09:58:30 +0200

    Aggiunta InetAddress nel costruttore del ClientHandler

--- src/main/java/org/example/ClientHandler.java
+++ src/main/java/org/example/ClientHandler.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.Socket;
 import java.util.Objects;
 
@@ -13,6 +14,8 @@ public class ClientHandler {
 
     public ClientHandler(Socket clientSocket) {
         this.clientSocket = clientSocket;
+        InetAddress inetAddress = this.clientSocket.getInetAddress();
+        System.out.println(inetAddress);
     }
     boolean manage() {
 
