c196e2c3bf68bdbb7f26cae0dc26d155abc9550d
GiacomoVilla14 <g.villa@itsrizzoli.it>
2023-06-16 10:33:14 +0200

    Creazione classe Command

---
+++ src/main/java/org/example/Command.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Command {
+    String cmd;
+    String param1;
+    String param2;
+
+    public Command(String cmd, String param1, String param2) {
+        this.cmd = cmd;
+        this.param1 = param1;
+        this.param2 = param2;
+    }
+
+    /*
+    es:list
+    {"cmd":"list"}
+
+
+    es:login
+    {"cmd":"login", "param1":"john", "param2":"doe"}
+     */
+}
